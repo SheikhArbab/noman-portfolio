479e4ce0296025b4f1d3ce750e051ec39cea7ae3
SheikhArbab <arbabzafar4444@gmail.com>
2024-09-15 02:21:01 +0500

    complete what i do

--- src/components/WhatIDo.tsx
+++ src/components/WhatIDo.tsx
@@ -14,16 +14,21 @@ const WhatIDo: React.FC = () => {
         { title: "UX/UI Design", Icon: SiMaterialdesignicons },
         { title: "Mobile App Design", Icon: CiMobile3 },
         { title: "Website Design", Icon: CgWebsite },
+        { title: "Brand Design", Icon: FaRegLightbulb },
         { title: "Brand Design", Icon: FaRegLightbulb }
     ]
 
     return (
         <C.Section className='py-8 md:py-20' id='what'>
             <h1 className="text-center font-bold text-2xl md:text-7xl ">What I Do ?</h1>
-            <ul className='flex flex-wrap items-center justify-between gap-8 py-5 md:py-24' >
-                {cardData.map(v => <li key={v.title} className='border border-black/90 h-40 w-96 p-8' >
-                    <v.Icon size={50} />
-                    <h1 className='capitalize font-semibold text-2xl' > {v.title}</h1>
+            <ul className='flex flex-wrap items-center justify- gap-3 md:gap-8 py-5 md:py-24' >
+                {cardData.map(v => <li
+                    key={v.title}
+                    className='border border-black/90 h-40 w-full lg:w-[25rem] p-8 flex flex-col items-start gap-4 duration-300 transition-all
+                    group overflow-hidden hover:rotate-12 hover:bg-gray-200'
+                >
+                    <v.Icon size={50} className='group-hover:-translate-y-10 group-hover:opacity-0 duration-500 transition-all' />
+                    <h1 className='capitalize font-semibold text-sm md:text-2xl' > {v.title}</h1>
                 </li>)}
                 <li></li>
             </ul>
