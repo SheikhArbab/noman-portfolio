964208b29a00bd57af650d281a7fdb076f51aa0e
SheikhArbab <arbabzafar4444@gmail.com>
2024-09-15 04:00:13 +0500

    complete skill section

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -3,11 +3,12 @@ import React, { ReactNode } from 'react';
 interface SectionProps {
     children: ReactNode;
     className?: string;
+    overflowhidden?: boolean;
     id?: string;
 }
 
-const Section: React.FC<SectionProps> = ({ children, className = '', id = '' }) => <>
-    <section id={id} className={`container px-2 mx-auto overflow-hidden ${className}`}>
+const Section: React.FC<SectionProps> = ({ children, className = '', id = '', overflowhidden = true }) => <>
+    <section id={id} className={`relative container px-2 mx-auto ${overflowhidden && 'overflow-hidden'} ${className}`}>
         {children}
     </section>
     <hr className='border-0  border-b-2 border-black/90' />
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -7,40 +7,42 @@ const Skills: React.FC = () => {
         title: string;
         count: number;
     }[] = [
-            {
-                title: "Figma",
-                count: 93
-            },
-            {
-                title: "Adobe XD",
-                count: 96
-            },
-            {
-                title: "Adobe Illustrator",
-                count: 94
-            },
-            {
-                title: "Adobe Photoshop",
-                count: 92
-            },
-            {
-                title: "Wondershare Filmora",
-                count: 93
-            },
-            {
-                title: "Adobe Premier Pro",
-                count: 9
-            }
-        ]
+            { title: "Figma", count: 93 },
+            { title: "Adobe XD", count: 96 },
+            { title: "Adobe Illustrator", count: 94 },
+            { title: "Adobe Photoshop", count: 92 },
+            { title: "Wondershare Filmora", count: 93 },
+            { title: "Adobe Premier Pro", count: 9 }
+        ];
     return (
-        <C.Section className='py-8 md:py-20' id='skills'>
-            {
-                skills.map(v => <div key={v.title}>
-                    <ProgressBar completed={v.count} bgColor='#000000c9' />
-                </div>)
-            }
+        <C.Section
+            className='py-8 md:py-20'
+            id='skills'
+            overflowhidden={false}
+        >
+            <div className='flex flex-wrap gap-5 relative'>
+                <div className='lg:sticky top-4 left-0 h-fit md:w-[50%] '>
+                    <h1 className="font-bold text-2xl md:text-7xl mb-5 md:mb-20 ">
+                        Special Skills
+                    </h1>
+                    <p className='text-xs md:text-xl md:w-[70%] opacity-90'>
+                        My skill set is complemented by a strong command of industry-standard tools such as Adobe XD, Figma, Sketch, Adobe Photoshop, and Adobe Illustrator. Whether it’s refining the finer details of a visual design or executing complex graphics, I leverage these tools to deliver polished and professional outcomes. Additionally, my capabilities extend to Graphic Design, where I blend creativity with strategic thinking to produce visually compelling designs that align with business objectives.
+                    </p>
+                </div>
+                <div className='flex-1 space-y-20 font-semibold p-5'>
+                    {skills.map(v => (
+                        <div key={v.title}>
+                            <p className='mb-2 text-sm md:text-2xl'>{v.title}</p>
+                            <div className='relative'>
+                                <sup className='absolute right-0 -top-10 text-sm md:text-2xl'>{v.count}%</sup>
+                                <ProgressBar height='6px' labelClassName="text-transparent" completed={v.count} bgColor='#000000c9' />
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </C.Section>
-    )
-}
+    );
+};
 
-export default Skills
+export default Skills;
