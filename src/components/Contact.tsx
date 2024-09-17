d4c853bb50fb69295fc553779edb1658c70ea984
SheikhArbab <arbabzafar4444@gmail.com>
2024-09-17 20:50:56 +0500

    test

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,14 +1,83 @@
-import React from 'react';
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
+import React, { useState } from 'react';
 import * as C from "@/components/index";
 import G from "@/constants/index";
+import emailjs from "@emailjs/browser";
+import { useToast } from "@/hooks/use-toast"
+
+
 
 const Contact: React.FC = () => {
+
+    const { toast } = useToast();
+
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    const { handleChange, handleSubmit, handleBlur, values, errors, touched } = useFormik({
+
+        initialValues: {
+            name: "",
+            email: "",
+            message: "",
+
+        },
+        enableReinitialize: true,
+        validationSchema: Yup.object({
+            name: Yup.string()
+                .required('name is required'),
+            email: Yup.string()
+                .email('Invalid email address')
+                .required('Email is required'),
+            message: Yup.string()
+                .min(3).max(500)
+                .required('Message is required'),
+        }),
+        onSubmit: async (formValues, { resetForm }) => {
+            setIsLoading(true);
+            try {
+
+                emailjs
+                    .send(
+                        "service_loiw3ya",
+                        "template_xxus1r9",
+                        {
+                            from_name: formValues.name,
+                            from_email: formValues.email,
+                            to_name: "JavaScript Mastery",
+                            to_email: "jnuman99@gmail.com",
+                            message: formValues.message,
+                        },
+                        "VnGa5n8caGIzrG3vV"
+                    )
+                    .then(
+                        () => {
+                            setIsLoading(false);
+                            toast({ title: "Thank you. I will get back to you as soon as possible." })
+                            resetForm();
+                        },
+                        (error) => {
+                            setIsLoading(false);
+                            console.error(error);
+                            toast({ variant: "destructive", title: "Ahh, something went wrong. Please try again." })
+                        }
+                    );
+            } catch (error: any) {
+                console.log(error);
+
+                toast({ variant: "destructive", title: "Something Went Wrong!" })
+            }
+        },
+
+    });
+
+
     return (
         <C.Section className='py-8 md:py-20' id='contact'>
             <h1 className="text-center mb-20 font-bold text-2xl md:text-7xl ">
                 Get In Touch With Me
             </h1>
-            <form className='flex flex-wrap-reverse gap-8 bg-black/80 px-6 py-8 md:p-20 rounded-lg'>
+            <form onSubmit={handleSubmit} className='flex flex-wrap-reverse gap-8 bg-black/80 px-6 py-8 md:p-20 rounded-lg'>
                 <div className='w-full md:w-1/2'>
                     <h1 className='text-white text-xl md:text-2xl font-semibold' >Contact Me</h1>
                     <ul className="flex flex-col gap-8 text-sm my-4 md:my-12" >
@@ -42,20 +111,44 @@ const Contact: React.FC = () => {
                     <h1 className='text-white mb-4 text-xl md:text-2xl md:mb-12 font-semibold' >Send Me a Message</h1>
                     <div className="flex flex-col gap-8 ">
                         <input type="text"
+                            value={values.name}
+                            onBlur={handleBlur}
+                            onChange={handleChange}
                             name='name'
                             placeholder='Name'
                             className='w-full block text-sm rounded-md px-4 py-2 focus:outline-none' />
+                        {touched.name && errors.name && (
+                            <p className="text-red-500 text-xs ">{errors.name}</p>
+                        )}
+                        <input type="email"
+                            value={values.email}
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            name='email'
+                            placeholder='example@email.com'
+                            className='w-full block text-sm rounded-md px-4 py-2 focus:outline-none' />
+                        {touched.email && errors.email && (
+                            <p className="text-red-500 text-xs ">{errors.name}</p>
+                        )}
                         <textarea
+                            value={values.message}
+                            onBlur={handleBlur}
+                            onChange={handleChange}
                             name="message"
                             placeholder='Message'
                             className='w-full block text-sm rounded-md px-4 py-2 focus:outline-none'
                         >
 
                         </textarea>
+                        {touched.message && errors.message && (
+                            <p className="text-red-500 text-xs ">{errors.message}</p>
+                        )}
                         <C.Button
-                            type='submit'
-                            className='w-full text-sm rounded-md' >
-                            send
+                            isLoading={isLoading}
+                            type="submit"
+                            className="w-full  focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5"
+                        >
+                            Send
                         </C.Button>
                     </div>
                 </div>
